21696a5efc66bbc1612be10fa3b3b9a837f1a8a4
Andrea Lovewell <andy.lovewell@gmail.com>
2019-02-19 20:37:21 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,19 @@
 class Artist
 
-  attr_accessor :name
+  attr_accessor :name, :artist, :genre
 
   @@all =[]
 
   def initialize(name)
     @name = name
   end
+
+  def. self.all
+    @all
+  end
   
+  def new_song(name, genre)
+    song = Song.new(name,self, genre)
+    self.songs.last
+  end
 end
