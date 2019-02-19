0902e63a0ebb78094bc1b782824d4c31ab01e56c
Andrea Lovewell <andy.lovewell@gmail.com>
2019-02-19 21:08:20 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,18 @@
+class Doctor
+
+@@all =[]
+
+def initialize(name)
+  @name = name
+  @@all << self
+end
+
+def self.all
+  @@all 
+end
+
+def new_appointment(patient, date)
+  Appointment.new(patient, date)
+end  
+
+end
